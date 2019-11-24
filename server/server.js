ca157a52870c2a62a5eb177c8a0ccfc6c1e3b3c9
Myron <dubtwee@gmail.com>
2019-11-24 12:29:25 -0600

    leveraging body-parser in application

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = 5000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log('Server running, listening on port: ', PORT);
