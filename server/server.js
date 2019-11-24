c3aadc183d11748d2961022a42d887fbf889c3ca
Myron <dubtwee@gmail.com>
2019-11-24 12:40:30 -0600

    creating get history endpoint on server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,22 @@ const PORT = 5000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+const history = [
+    // {
+    //     mathNum1: 0,
+    //     mathNum2: 0,
+    //     mathOperator: 'add', // 'sub', 'mutl', 'divi', 'add'
+    //     solution: 0
+    // }
+];
+
+// GET of the equation history
+app.get('/api/equation-history', (req, res) => {
+    res.send(history);
+});
+
+// POST that accepts an object for an equation
+
 app.listen(PORT, () => {
     console.log('Server running, listening on port: ', PORT);
 });
